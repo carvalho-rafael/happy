1b443bc78bd2c3c803c8fee1620b96d2830caa51
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2020-10-14 00:03:21 -0300

    create Landing page and upload images

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import {Link} from 'react-router-dom'
+import {FiArrowRight} from 'react-icons/fi'
+import logo from '../images/logo.svg'
+
+import '../styles/pages/landing.css'
+
+function Landing() {
+    return (
+        <div className="App" id="page-landing">
+        <div className="content-wrapper">
+          <header>
+            <img src={logo} alt="happy-logo" />
+            <div>
+              <strong>Salvador - </strong><span>Bahia</span>
+            </div>
+          </header>
+  
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+            <Link to="/app" className="link-go-app">
+              <FiArrowRight size="30px" color="rgba(0,0,0, .8)"/>
+            </Link>
+          </main>
+        </div>
+      </div>
+    )
+}
+
+export default Landing;
